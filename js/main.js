7dfee8b2d4f500b8521721616e4461bed8f4a0c5
GarridoEri <106647575+GarridoEri@users.noreply.github.com>
2023-01-30 15:35:24 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-let btnMenu = document.getElementById('btnmenu');
-let menu = document.getElementById('menu');
-btnMenu.addEventListener('click',function(){
-    'use strict';
-    menu.classList.toggle('mostrar');
-});
-
+// let btnMenu = document.getElementById('btnmenu');
+// let menu = document.getElementById('menu');
+// btnMenu.addEventListener('click',function(){
+//     'use strict';
+//     menu.classList.toggle('mostrar');
+// });
 function desplegar_menu($menu){
     $('.menu-section').css('display', 'none');
     $('#'+$menu).css('display', 'block');
